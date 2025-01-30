2ad21be6992647c0a35ab90564be1befa6b4498d
Nokhalal Mahato <nokhalal.mahato@nxtwave.co.in>
2025-01-30 15:58:56 +0530

    added header

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -292,14 +292,24 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
         <div ref={scrollRef} className="flex flex-col lg:flex-row overflow-y-auto w-full h-full">
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
-              <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  Got an idea?
-                </h1>
-                <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  Build it in seconds and bring your vision to life effortlessly
-                </p>
-              </div>
+              <>
+                <div id="header" className="flex flex-col items-center justify-center w-auto">
+                  <h2 className="text-3xl lg:text-6xl text-blue-900 font-bold  mb-4 animate-fade-in !mb-0">
+                    Prompt2UI
+                  </h2>
+                  <p className="text-md lg:text-xl mb-8 text-blue-900 animate-fade-in animation-delay-200 align-left">
+                    Powered by <b>NXTWAVE</b>
+                  </p>
+                </div>
+                <div id="intro" className="max-w-chat mx-auto text-center px-4 lg:px-0">
+                  <h3 className="text-3xl lg:text-3xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
+                    Got an idea?
+                  </h3>
+                  <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
+                    Build it in seconds and bring your vision to life effortlessly
+                  </p>
+                </div>
+              </>
             )}
             <div
               className={classNames('pt-6 px-2 sm:px-6', {
